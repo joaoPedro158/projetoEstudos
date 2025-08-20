53b327931cc2488259827d80fa9fd0e721390a06
J0a0p3dr067 <pjpedro158@gmail.com>
2025-08-20 16:17:40 -0300

    adição de um footer na aplicação

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/Cores.css') }}">
     <link rel="stylesheet" href="{{ asset('css/padrao.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -140,8 +141,63 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <script src="/js/script.js"></script>
-    <footer>
-        <p>&copy; {{ date('Y') }} Projeto Estudos. All rights reserved.</p>
+
+     <footer class="footer-bazzary py-5">
+        <div class="container">
+            <div class="row">
+                <!-- Coluna 1: Atendimento ao Cliente -->
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="footer-title">Atendimento ao Cliente</h5>
+                    <ul class="footer-links">
+                        <li><a href="#">Central de Ajuda</a></li>
+                        <li><a href="#">Como comprar</a></li>
+                        <li><a href="#">Como vender</a></li>
+                        <li><a href="#">Devolução e reembolso</a></li>
+                        <li><a href="#">Garantia Bazzary</a></li>
+                        <li><a href="#">Fale conosco</a></li>
+                    </ul>
+                </div>
+
+                <!-- Coluna 2: Sobre o Bazzary -->
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="footer-title">Sobre o Bazzary</h5>
+                    <ul class="footer-links">
+                        <li><a href="#">Sobre nós</a></li>
+                        <li><a href="#">Política do Bazzary</a></li>
+                        <li><a href="#">Política de privacidade</a></li>
+                        <li><a href="#">Afiliados</a></li>
+                        <li><a href="#">Como ser um vendedor</a></li>
+                        <li><a href="#">Bazzary Blog</a></li>
+                    </ul>
+                </div>
+
+                <!-- Coluna 3: Siga-nos -->
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="footer-title">Siga-nos</h5>
+                    <ul class="footer-links social-links">
+                        <li><a href="#"><i class="bi bi-instagram"></i> Instagram</a></li>
+                        <li><a href="#"><i class="bi bi-tiktok"></i> Tiktok</a></li>
+                        <li><a href="#"><i class="bi bi-whatsapp"></i> Whatsapp</a></li>
+                        <li><a href="#"><i class="bi bi-facebook"></i> Facebook</a></li>
+                        <li><a href="#"><i class="bi bi-linkedin"></i> Linkedin</a></li>
+                    </ul>
+                </div>
+
+                <!-- Coluna 4: Temporada -->
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="footer-title">Temporada</h5>
+                    <ul class="footer-links">
+                        <li><a href="#">Dia das Mães</a></li>
+                        <li><a href="#">Dia do consumidor</a></li>
+                        <li><a href="#">Dia dos namorados</a></li>
+                        <li><a href="#">Black Friday</a></li>
+                        <li><a href="#">Descontão</a></li>
+                        <li><a href="#">Fale conosco</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
     </footer>
 </body>
 
