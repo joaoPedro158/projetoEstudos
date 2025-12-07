0ce74d6ae131cdde9a7e8be6a0bde66eb2e09803
J0a0p3dr067 <pjpedro158@gmail.com>
2025-12-07 11:57:44 -0300

    fix: trucamento de texto para descrição muito grande

--- resources/views/components/produto/produto-card.blade.php
+++ resources/views/components/produto/produto-card.blade.php
@@ -1,24 +1,25 @@
 
     <div style="max-width: 300px;">
     <a href="/produto/{{ $produto->id }}" class="text-decoration-none text-reset">
-        <div class="card h-100 shadow-sm border-0 text-decoration-none text-dark">
+        <div class="border-0 shadow-sm card h-100 text-decoration-none text-dark">
             {{-- Imagem do Produto --}}
-            <img src="{{ asset('storage/' . $produto->imagem) }}" class="card-img-top p-3" alt="{{ $produto->nome }}">
+            <img src="{{ asset('storage/' . $produto->imagem) }}" class="p-3 card-img-top" alt="{{ $produto->nome }}">
 
             {{-- Descrição do Produto --}}
-            <div class="px-3 pb-2"> 
-                <p class="text-muted small mb-0">{{ $produto->descricao }}</p>
+            <div class="px-3 pb-2">
+                <p class="mb-0 text-muted small truncate-text">{{ $produto->descricao }}</p>
             </div>
 
             <div class="card-body d-flex flex-column">
                 {{-- Nome do Produto --}}
-                <h5 class="card-title fs-6">{{ $produto->nome }}</h5>
+
 
                 {{-- Preço do Produto --}}
                 <div class="mt-auto">
+                    <h5 class="card-title fs-6">{{ $produto->nome }}</h5>
                     <div class="mb-2">
                         <span class="h3 fw-bold">R$ {{ number_format($produto->preco, 2, ',', '.') }}</span>
-                        <p class="text-muted small mb-0">em 12x de R$ {{ number_format($produto->preco / 12, 2, ',', '.') }}</p>
+                        <p class="mb-0 text-muted small">em 12x de R$ {{ number_format($produto->preco / 12, 2, ',', '.') }}</p>
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
                         <span style="color: #587DBD; font-weight: bold;">Frete Grátis</span>
