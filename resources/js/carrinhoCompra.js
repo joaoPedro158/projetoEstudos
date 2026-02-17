5f52ee16c2e2937f8520f9b052b3008282fc6730
J0a0p3dr067 <pjpedro158@gmail.com>
2026-02-17 20:40:55 -0300

    refatoração
    retroando um valor em preço

--- resources/js/carrinhoCompra.js
+++ resources/js/carrinhoCompra.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("linkando");
+
 
     const produtosJson = JSON.parse(document.getElementById('produtosEloquet').dataset.produtos);
     console.log(produtosJson);
 
+    function precoProdutos(produtos) {
+        let total = 0;
+        produtos.forEach(produto => {
+            let valorNumerico = parseFloat(produto.preco)
+            total += valorNumerico;
+        });
+        console.log(total);
+
+        let totalformatado = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(total);
+        return totalformatado;
+    }
+
+    let total =precoProdutos(produtosJson);
+    console.log(total);
 });
 
