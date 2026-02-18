472678fc60d3583f3f281a8bda8529be0b455abd
J0a0p3dr067 <pjpedro158@gmail.com>
2026-02-18 19:22:36 -0300

    feat: o preço total é calculado pela checkbox marcadas

--- resources/js/carrinhoCompra.js
+++ resources/js/carrinhoCompra.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const precoPrdutos = document.getElementById('precoProdutos');
 
-    const produtosJson = JSON.parse(document.getElementById('produtosEloquet').dataset.produtos);
-    console.log(produtosJson);
 
-    function precoProdutos(produtos) {
+    function calcularTotal() {
         let total = 0;
-        produtos.forEach(produto => {
-            let valorNumerico = parseFloat(produto.preco)
-            total += valorNumerico;
-        });
-        console.log(total);
+        const checkBoxMarcados = document.querySelectorAll('.form-check-input:checked');
+
+        checkBoxMarcados.forEach(checkbox => {
+            let preco = parseFloat(checkbox.getAttribute('data-preco')) || 0;
+            total += preco;
+        })
 
         let totalformatado = new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
         }).format(total);
+
+        precoPrdutos.textContent = totalformatado;
+        document.querySelectorAll('.form-check-input').forEach(checkbox => {
+            checkbox.addEventListener('change', calcularTotal);
+        });
+
         return totalformatado;
     }
 
-    let total =precoProdutos(produtosJson);
-    console.log(total);
+
+    calcularTotal();
+
+
 });
 
